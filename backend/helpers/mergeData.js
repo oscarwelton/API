e9994d65b887987ad020b5c152de2846906dc2a0
oscar welton <oscarwelton@gmail.com>
2023-09-08 10:24:49 +0100

    merging of antonyms and synonyms successful

--- backend/helpers/mergeData.js
+++ backend/helpers/mergeData.js
@@ -1,23 +1,44 @@
+const fs = require("fs");
+
 const antonyms = require("../data/JSON/antonyms.json");
 const synonyms = require("../data/JSON/synonyms.json");
 
 const synonymWords = synonyms.map((object) => object.word.toLowerCase().trim());
 const antonymWords = antonyms.map((object) => object.word.toLowerCase().trim());
 
-const mergedJSON = {};
+const mergedJSON = [];
 
 synonymWords.forEach((word) => {
+  let merged = {};
   const matchedIndex = antonymWords.indexOf(word);
   if (matchedIndex !== -1) {
     const matchingSynonym = synonyms.find(
       (synonym) => synonym.word.toLowerCase().trim() === word
     );
     const matchingAntonym = antonyms[matchedIndex];
-    mergeObject(matchingSynonym, matchingAntonym)
+    merged = mergeObject(matchingSynonym, matchingAntonym);
+  } else {
+    merged = mergeObject(
+      synonyms.find((synonym) => synonym.word.toLowerCase().trim() === word),
+      { antonyms: [] }
+    );
   }
+  mergedJSON.push(merged);
 });
 
-
 function mergeObject(synonym, antonym) {
-  console.log(synonym, antonym)
+  const word = synonym.word;
+  const mergedObject = {
+    [word]: {
+      pos: synonym.pos,
+      synonyms: synonym.synonyms,
+      antonyms: antonym.antonyms,
+    },
+  };
+  return mergedObject;
 }
+
+fs.writeFileSync(
+  "../data/JSON/merged.json",
+  JSON.stringify(mergedJSON, null, 2)
+);
